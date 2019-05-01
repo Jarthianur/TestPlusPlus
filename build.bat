386536ff514d39db08d4a946508c9caf07a285ea
Jarthianur <jarthianur.github@gmail.com>
2019-05-01 11:37:35 +0200

    fix pipeline and reporting

--- build.bat
+++ build.bat
@@ -1,13 +1,13 @@
 @echo off
 
 where cl >nul 2>nul
-IF %ERRORLEVEL% NEQ 0 (echo WARNING: cl is not in the path - please set up Visual Studio to do cl builds) && exit 1
+IF %ERRORLEVEL% NEQ 0 (echo ERROR: cl is not in PATH) && exit 1
 
 pushd test
-cl /GS /W3 /Zi /Od /FC /EHsc /nologo                    main.cpp reflexive_tests.cpp basic_tests.cpp /link /out:cpp11_seq.exe
-cl /GS /W3 /Zi /Od /FC /EHsc /nologo            /openmp main.cpp reflexive_tests.cpp basic_tests.cpp /link /out:cpp11_par.exe
-cl /GS /W3 /Zi /Od /FC /EHsc /nologo /std:c++14         main.cpp reflexive_tests.cpp basic_tests.cpp /link /out:cpp14_seq.exe
-cl /GS /W3 /Zi /Od /FC /EHsc /nologo /std:c++14 /openmp main.cpp reflexive_tests.cpp basic_tests.cpp /link /out:cpp14_par.exe
-cl /GS /W3 /Zi /Od /FC /EHsc /nologo /std:c++17         main.cpp reflexive_tests.cpp basic_tests.cpp /link /out:cpp17_seq.exe
-cl /GS /W3 /Zi /Od /FC /EHsc /nologo /std:c++17 /openmp main.cpp reflexive_tests.cpp basic_tests.cpp /link /out:cpp17_par.exe
+cl /GS /W3 /Zi /Od /FC /EHsc /nologo /I..\include                    main.cpp reflexive_tests.cpp basic_tests.cpp /link /out:cpp11_seq.exe
+cl /GS /W3 /Zi /Od /FC /EHsc /nologo /I..\include            /openmp main.cpp reflexive_tests.cpp basic_tests.cpp /link /out:cpp11_par.exe
+cl /GS /W3 /Zi /Od /FC /EHsc /nologo /I..\include /std:c++14         main.cpp reflexive_tests.cpp basic_tests.cpp /link /out:cpp14_seq.exe
+cl /GS /W3 /Zi /Od /FC /EHsc /nologo /I..\include /std:c++14 /openmp main.cpp reflexive_tests.cpp basic_tests.cpp /link /out:cpp14_par.exe
+cl /GS /W3 /Zi /Od /FC /EHsc /nologo /I..\include /std:c++17         main.cpp reflexive_tests.cpp basic_tests.cpp /link /out:cpp17_seq.exe
+cl /GS /W3 /Zi /Od /FC /EHsc /nologo /I..\include /std:c++17 /openmp main.cpp reflexive_tests.cpp basic_tests.cpp /link /out:cpp17_par.exe
 popd
